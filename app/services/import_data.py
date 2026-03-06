7376db038d7e224c67ba97907d053703ea43d5b1
sc23ls <sc23ls@leeds.ac.uk>
2026-03-06 14:46:48 +0000

    Fixed dataset issue (outdated)

--- app/services/import_data.py
+++ app/services/import_data.py
@@ -1,28 +1,40 @@
 import pandas as pd
 from datetime import datetime
-from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
 from app.models.exchange_rate import ExchangeRate
 
+print("IMPORT SCRIPT STARTED")
 
 def import_ecb_data(csv_path="data/eurofxref-hist.csv"):
 
     df = pd.read_csv(csv_path)
 
-    db: Session = SessionLocal()
+    db = SessionLocal()
 
     for _, row in df.iterrows():
+
         date = datetime.strptime(row["Date"], "%Y-%m-%d")
 
-        for currency, rate in row.items():
-            if currency == "Date" or pd.isna(rate):
+        for currency in df.columns:
+
+            if currency == "Date":
+                continue
+
+            rate = row[currency]
+
+            if pd.isna(rate):
+                continue
+
+            try:
+                rate = float(rate)
+            except:
                 continue
 
             exchange = ExchangeRate(
                 base_currency="EUR",
                 target_currency=currency,
-                rate=float(rate),
+                rate=rate,
                 date=date
             )
 
@@ -31,8 +43,7 @@ def import_ecb_data(csv_path="data/eurofxref-hist.csv"):
     db.commit()
     db.close()
 
-    print("Data import complete.")
-
+    print("Data import complete")
 
 if __name__ == "__main__":
     import_ecb_data()
